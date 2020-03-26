e2dfbc89b112e99f0a05d51b39f5a04e8c725496
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-03-26 12:00:16 +0530

    commit

--- Corona Virus Prediction/predict.py
+++ Corona Virus Prediction/predict.py
@@ -41,14 +41,14 @@ world_recovered = np.array(world_recovered).reshape(-1, 1)
 days_in_future = 10
 c = len(dates)
 future_forcast = np.array([i+c for i in range(days_in_future)]).reshape(-1, 1)
-adjusted_dates = future_forcast[:-10]
+#adjusted_dates = future_forcast[:-10]
 
 
-start = '1/22/2020'
+"""start = '1/22/2020'
 start_date = datetime.datetime.strptime(start, '%m/%d/%Y')
 future_forcast_dates = []
 for i in range(len(future_forcast)):
-    future_forcast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
+    future_forcast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))"""
 
 """from sklearn.model_selection import train_test_split
 X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(days_since_1_22, world_cases, test_size=0.15, shuffle=False)""" 
